54c5eaac03e6e9e11e216fc8db60d66dcc678301
bmsimon <besi2977@colorado.edu>
2024-08-28 10:18:56 -0600

    Create dialogue1.cpp

---
+++ dialogue1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>   // DO NOT TOUCH THIS LINE OF CODE
+using namespace std;  // DO NOT TOUCH THIS LINE OF CODE
+
+int main() {	      // DO NOT TOUCH THIS LINE OF CODE
+	
+    /*INSTRUCTIONS: For this assignment, move the lines of code (Lines 14 - 22) 
+                    into the correct sequential order to generate the response 
+                    provided on the slides. Do not modify any of the lines of 
+                    code, only switch their order. Keep this version of the
+                    code untouched, but copy/paste the lines in their correct
+                    order in the file called dialogue1.cpp.
+    */ 
+    
+    cout << "Barista: Good morning! What can I get for you today?" << endl;
+
+    cout << "Customer: Good morning! I'd like a medium cappuccino, please." << endl;
+       
+    cout << "Barista: Sure thing. Would you like any pastries with that?" << endl;
+
+    cout << "Customer: Yes, I'll have a blueberry muffin, please." << endl;
+    
+    cout << "Barista: Great choice! Your cappuccino and muffin will be ready shortly." << endl;
+    
+
+ 
+    return 0;   	// DO NOT TOUCH THIS LINE OF CODE
+}					// DO NOT TOUCH THIS LINE OF CODE
